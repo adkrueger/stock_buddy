a03d883908cb8c927efb1e8c49a454d42175f5d6
adkrueger <adkrueger@wpi.edu>
2020-05-24 15:06:00 -0400

    found soup parse for next 5 year growth

--- stock_scraper.py
+++ stock_scraper.py
@@ -2,19 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def scrape_url(url):
-    response = None
+def scrape_url(ticker):
+    yahooURL = 'https://finance.yahoo.com/quote/' + ticker + '/analysis?p=' + ticker
+
     try:
-        response = requests.get(url)
-        response.raise_for_status()
+        yahooResp = requests.get(yahooURL)
+        yahooResp.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(e)
         print('\nPlease refresh the page and try again')
         exit()
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(yahooResp.text, 'html.parser')
     growth_last5 = soup.find_all('tr')[-1].find_all('td')[1]
+    growth_next5 = soup.find_all('tr')[-2].find_all('td')[1]
     print(growth_last5.text)
+    print(growth_next5.text)
 
 
-scrape_url('https://finance.yahoo.com/quote/MSFT/analysis?p=MSFT')
+scrape_url('MSFT')
