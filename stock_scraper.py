b5e5f3276cde250a41c1702ff8d4078331e5cae1
adkrueger <adkrueger@wpi.edu>
2020-05-29 22:15:31 -0400

    figured out soup to get ROIC from guru

--- stock_scraper.py
+++ stock_scraper.py
@@ -16,10 +16,14 @@ def scrape_url(ticker):
         exit()
 
     yahooSoup = BeautifulSoup(yahooResp.text, 'html.parser')
+    guruSoup = BeautifulSoup(guruResp.text, 'html.parser')
     growth_last5 = yahooSoup.find_all('tr')[-1].find_all('td')[1]
     growth_next5 = yahooSoup.find_all('tr')[-2].find_all('td')[1]
+    roic = guruSoup.find_all(class_='bar-divider')
+    roic = [div.text[6:-1] for div in roic if 'ROIC' in div.text][0]
     print(growth_last5.text)
     print(growth_next5.text)
+    print(roic)
 
 
 scrape_url('MSFT')
