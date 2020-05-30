d372746c37d57a02d37e1a9e0a4605478b92800f
adkrueger <adkrueger@wpi.edu>
2020-05-29 22:26:21 -0400

    figured out soup to get dte from mb

--- stock_scraper.py
+++ stock_scraper.py
@@ -5,11 +5,14 @@ from bs4 import BeautifulSoup
 def scrape_url(ticker):
     yahooURL = 'https://finance.yahoo.com/quote/' + ticker + '/analysis?p=' + ticker
     guruURL = 'https://www.gurufocus.com/stock/' + ticker + '/summary'
+    mbURL = 'https://www.marketbeat.com/stocks/' + ticker + '/'
     try:
         yahooResp = requests.get(yahooURL)
         yahooResp.raise_for_status()
         guruResp = requests.get(guruURL)
         guruResp.raise_for_status()
+        mbResp = requests.get(mbURL)
+        mbResp.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(e)
         print('\nPlease refresh the page and try again')
@@ -17,13 +20,22 @@ def scrape_url(ticker):
 
     yahooSoup = BeautifulSoup(yahooResp.text, 'html.parser')
     guruSoup = BeautifulSoup(guruResp.text, 'html.parser')
+    mbSoup = BeautifulSoup(mbResp.text, 'html.parser')
+
+    # growth in last 5 years and next 5 years
     growth_last5 = yahooSoup.find_all('tr')[-1].find_all('td')[1]
     growth_next5 = yahooSoup.find_all('tr')[-2].find_all('td')[1]
+    # return on invested capital
     roic = guruSoup.find_all(class_='bar-divider')
     roic = [div.text[6:-1] for div in roic if 'ROIC' in div.text][0]
+    # debt to equity ratio
+    dte = mbSoup.find_all(class_='price-data')
+    dte = [div.text[20:] for div in dte if 'Debt-to-Equity' in div.text][0]
+
     print(growth_last5.text)
     print(growth_next5.text)
     print(roic)
+    print(dte)
 
 
 scrape_url('MSFT')
