7ee335f90855ec9279820b515c6c3b9abe1c98cf
adkrueger <adkrueger@wpi.edu>
2020-05-29 22:06:06 -0400

    fixed scraper to be more friendly to other URLs

--- stock_scraper.py
+++ stock_scraper.py
@@ -4,18 +4,20 @@ from bs4 import BeautifulSoup
 
 def scrape_url(ticker):
     yahooURL = 'https://finance.yahoo.com/quote/' + ticker + '/analysis?p=' + ticker
-
+    guruURL = 'https://www.gurufocus.com/stock/' + ticker + '/summary'
     try:
         yahooResp = requests.get(yahooURL)
         yahooResp.raise_for_status()
+        guruResp = requests.get(guruURL)
+        guruResp.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(e)
         print('\nPlease refresh the page and try again')
         exit()
 
-    soup = BeautifulSoup(yahooResp.text, 'html.parser')
-    growth_last5 = soup.find_all('tr')[-1].find_all('td')[1]
-    growth_next5 = soup.find_all('tr')[-2].find_all('td')[1]
+    yahooSoup = BeautifulSoup(yahooResp.text, 'html.parser')
+    growth_last5 = yahooSoup.find_all('tr')[-1].find_all('td')[1]
+    growth_next5 = yahooSoup.find_all('tr')[-2].find_all('td')[1]
     print(growth_last5.text)
     print(growth_next5.text)
 
