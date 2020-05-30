97ec2e513ad35a0517f7b97fc49dd6136d139dfa
adkrueger <adkrueger@wpi.edu>
2020-05-29 23:28:15 -0400

    removed imports

--- api_main.py
+++ api_main.py
@@ -1,8 +1,4 @@
 from config import CONFIG_KEY
-import datetime as dt
-import matplotlib.pyplot as plt
-from matplotlib import style
-import pandas as pd
 import pandas_datareader.data as pdr
 
 api_key = CONFIG_KEY
